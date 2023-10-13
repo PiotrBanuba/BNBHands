4f2ad202b4e135f84879a29ed2390dcc48a5b8e9
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-13 09:51:49 +0300

    1.8.0-10

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-9"
+let bnbPackageVersion: Version = "1.8.0-10"
 
 let package = Package(
     name: "BNBHands",
@@ -13,9 +13,9 @@ let package = Package(
             name: "BNBHands",
             targets: [
                 "BNBHands",
-                "BNBHandsBNBSdkCore",
-                "BNBHandsBNBEffectPlayer",
-                "BNBHandsBNBScripting"
+                "BNBSdkCore",
+                "BNBEffectPlayer",
+                "BNBScripting"
             ]
         ),
     ],
@@ -38,33 +38,6 @@ let package = Package(
             name: "BNBHands",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBHands.zip",
             checksum: "723e6297ea1a302c5e71e9317aebca48ae8f71c7887d03c52d0a2a3d3b0bc36a"
-        ),
-        .target(
-            name: "BNBHandsBNBSdkCore",
-            dependencies: [
-                .product(
-                    name: "BNBSdkCore",
-                    package: "BNBSdkCore"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBHandsBNBEffectPlayer",
-            dependencies: [
-                .product(
-                    name: "BNBEffectPlayer",
-                    package: "BNBEffectPlayer"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBHandsBNBScripting",
-            dependencies: [
-                .product(
-                    name: "BNBScripting",
-                    package: "BNBScripting"
-                ),
-            ]
         ),
     ]
 )
